8aef7f879758935be2f39a4e2bc5ff4b1efbd213
mehtaViraj <mehta.viraj.03@gmail.com>
2021-11-13 13:01:54 -0500

    Co-authored-by: Rocksen1683 <Rocksen1683@users.noreply.github.com>

---
+++ readInputs_bad_key.py
@@ -0,0 +1,80 @@
+'''
+1. 0.25s for conscecutive taps on the same number
+2. 0.5s to register the current letter so that another letter in the same number can be tapped
+3. 0.25s to move between letters. Ex: from d (tap 3) to g (tap 4)
+4. 2s for functionality like uppercase or special functionality like voicemail
+'''
+#Prepare Dict
+all_letters = ['abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+keypad = {}
+#Dict contains the key and no. of key presses required for each letter
+for i in range(len(all_letters)):
+    for j in range(len(all_letters[i])):
+        keypad[all_letters[i][j]] = (i+2, j)
+#print(keypad)
+
+#Read text file and remove the newline characters
+f = open("./test_txt/part2_test.txt", "rt")
+words = f.readlines()
+len_words = len(words)
+for i in range(len_words):
+    if words[i][-1] == '\n':
+        words[i] = words[i][:-1]
+#print(words)
+
+broken_key = int(words[0]) #First line is the broken key
+words = words[1:] #The remaining lines are words
+for i in range(len(all_letters[broken_key-2])):
+    if i<2:
+        keypad[all_letters[broken_key-2][i]] = ('*', (i % 2) + 1)
+    else:
+        keypad[all_letters[broken_key-2][i]] = ('#', (i % 2) + 1)
+#print(keypad)
+
+#Compute time for each word in the text file.
+time_taken = []
+best_words = [] #List of fastest words
+fastest_time = None #Fastest time
+for word in words:
+    word_time = 0
+    prev_key = -1
+    for i in word:
+        is_upper = i.isupper()
+        i = i.lower()
+        key, key_depth = keypad[i]
+
+        #Start timer
+        t = 0
+        if prev_key == -1: #Rule 1
+            pass
+        elif prev_key == key: #Rule 4
+            t = t + 0.5
+        else:
+            t = t + 0.25 #Rule 2
+        
+        t = t + (0.25*key_depth) #Rule 3
+
+        if is_upper: #Rule 5
+            t = t + 2
+        
+        word_time = word_time + t
+        prev_key = key #Memorize previous key for Rule 1, 2, and 4
+
+    if fastest_time == None:
+        best_words = [word]
+        fastest_time = word_time
+    elif fastest_time > word_time:
+        best_words = [word]
+        fastest_time = word_time
+    elif fastest_time == word_time:
+        best_words.append(word)
+
+    time_taken.append(word_time)
+    word_time = 0 # Clear word time for next word
+    prev_key = -1 # Clear previous key for next word
+
+for i in range(len(words)):
+    print('{} takes {} seconds'.format(words[i], time_taken[i]))
+
+print('------------------------------------------')
+print('The fastest word(s) are {} . They take {} seconds'.format(best_words, fastest_time))
